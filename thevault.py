3ba5284d8a036547ce5b616a6a4ead7d3f155bc0
Japanga <japanganemo@gmail.com>
2025-09-18 19:27:33 -0500

    Create thevault.py

---
+++ thevault.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def on_button_click():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "FOAZ (Father of All Zombies), sometimes also simply called The Zombie, is the name of a particularly mindbroken Brolyfag, whose hatred for CHADren exceeds all imagination. He is famous for flooding threads with shitty anti-Jiren bait and typing SHITren over 5000 times, according to desuarchive. Like all anti-Jiren jihadists, he thinks about Jiren all day, and will sperg out over CHADren even when nobody brought him up.[1] However, his levels of mindbrokenness go far beyond the average anti-Jiren jihadist: At some point, the detective work of the astute Jirenscholars determined that he was spamming his schizobabble non-stop for literally the entire day. Further, he will go through the effort of collecting any bait he can from irrelevant merchandise, simply because he has nothing else to do, as well as samefagging his own posts relentlessly.[2] He thus became known as The Zombie[3], due to his mindless nature and absence of reason, which made engaging with him in any way completely pointless. In the words of one Jirenscholar: There is no sanity left, no arguments to win, no glory to gain, and nothing to prove. Just a rabid, screeching animal that is the very product of our unending success.")
+def on_button_click2():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "POAZ (Prince of all Zombies), better known as The Belgian or B for short, is a title commonly associated with the Belgian BejitaBitch who has 600+ images of Goku jobbing saved on his computer, and would routinely flood entire threads with them, to the great anger of TARDkurats. For his insane obsession with Goku and frequent thread floodings, he became known as the POAZ, based on the infamous FOAZ. The newfaggot first began posting in the summer of 2020, and started using his filenames commonly named Prince of All - #. This extreme recognition - bordering on tripfagging and avtarfagging, led to him becoming the Public Enemy #1 of Tardkufags. To fight back against him, the Tardkufags began the heaviest falseflagging operation in the entire history of /dbs/, copying his filenames and attaching them to embarrassing posts that implied all Bejitabros were pedophile moeshitters. Because of this, he started randomizing his filenames and went into hiding. Despite all of this, The Belgian is a (somewhat) respected member of the community, for hosting streams for livewatches of Dragon Ball content. Ironically, POAZ himself gained an obsessed, mindbroken stalker who follows him through the site. This obsessed psycho came to be known as The Narrator for constantly greentexting his thoughts in third-person as if he was narrating a story. He will frequently claim that The Belgian is here, even when he isn't, which suggests that he suffers from Fregoli Delusion. Although POAZ is the name of the Belgian the moniker can still be used against other obsessed schizophrenics with a fixation on Goku. Currently, there is a Bejitafag that is perhaps even more mind broken than the Belgian.")
+def on_button_click3():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "Jiren (ジレン Jiren), also known as Jiren The Gray (灰色のジレン Haiiro no Jiren)[1] or CHADren, is the strongest mortal in the multiverse and the incarnation of Absolute Strength. He is known as the Invincible Strongest of the Strong,[2] the Strongest in all the Universes,[3] the Mortal who surpasses the Gods of Destruction,[4][5] the Alpha of Awe,[6][7] the Brute of Brawn,[6][7] the Cultivator of Class,[6] the Duke of Domination,[6] the Emperor of Eloquence,[6] the Fiercest of Fighters,[6] the Greatest of Glory,[6] the Height of Heroism,[6] the Imperator of Intellectualism,[6] the Jarl of Justice,[6] the King of Knights,[6] the Lord of Loquaciousness,[6] the Master of Mortality,[6] the Naysayer of Noobs,[6] the Overlord of Obituaries,[6] the Prince of Passion,[6] the Que-hagen of Quixote,[6] the Ruler of Ruination,[6] the Sultan of Smite,[6] the Taskmaster of Trembles,[6] the Undertaker of Ubiquity,[6] the Vaeyen of Vociferousness,[6] the Warranter of Weaklings,[6] the Xenophobe of Xenogeny,[6] the Yardmaster of Yesteryear,[6] the Zhar of Zoroastrianism,[6][7][8] the Apex Predator of the Multiverse,[9] GRIPren the CRUSHING,[10] INSURMOUNTABLEren the UNSURPASSABLE,[11] INDOMITABLEren the UNYIELDING,[11] DIAMONDren the TANK,[12][13][14] MAELSTROMren the TITANIC,[15] COLOSSUSren the LEVIATHAN[16], MONSTERren the TERRIFYING,[17] PHYSICALLYIMPOSINGren the INTIMIDATING,[18] AESTHETICren the BEAUTIFUL,[19] CRIMSONren the LEGEND,[20][21][22], The Alabaster Titan,[23][24] and many other names.")
+def on_button_click4():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "Romaine-Sama, CHADmaine or simply Romaine is the daughter of Turles and the official queen and goddess of /dbs/. Unlike popular thoughts, Romaine and her design was created before Caulifla's first concept art. The author was also a fan of Yuyu Hakusho and decided to give her Hiei's clothes. She is also an honorable member of the chadforce. Here is a quote from CHADliban himself: All great fistfights to the death are homoerotic, and the Devastators are deeply homoerotic to start with— the 80s glam rock BDSM style, the big sweaty muscles, the men obsessed with each other, often thinking obsessively about one another while half naked and declaring that they need one another for absolution, wrestling and grappling each others' muscly and sweaty bodies in struggles of dominance and strength. To that end, Yulaan actually ruins it by being female. She still does all the things that defines these sorts of stories— ripping off her shirt and grapple fighting sweaty and bloody muscle-bound men, bearing her own muscular body in the process. But the muscle worship and admiration of the male form is interrupted by her. In a world where females are scarce, where you can go months without seeing a woman or nonhuman female even while traveling extensively, where Devastator combat philosophy dictates powerful men must thrust themselves deep into the annals of bloody danger and risk and hold manly grudges against one another, where there are many shirtless fistfights between two men who passionately scream each other's name, where you're infinitely more likely to come across a girly rentboy than a maiden and the closest thing to a female warrior you'll get is an effeminate crossdressing eunuch. There's too much sausage, too much manhood, and the only girl on the entire planet presents a feint.")
+def on_button_click5():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "Reekdom, is an infamous figure in the CHADgon CHAD CHADper community. He is most well-known for frequenting his nearest Whataburger joint, and asserting the unconfirmed but not disproven hypothesis that the official canon anime made by CHADoei CHADnimation would return in July 2019. He claimed to be 200% sure this was going to happen, according to dozens of Japanese sources working at Toei. When he inevitably got BTFO because the anime did NOT return he, instead of gracefully kneeling, decided to double down on his lies and proclaim he still wasn't wrong. One of the pathetic excuses he came up with was that Toei Animators suddenly all got drafted to finish the latest Wan Piss moeshit movie, completely ignoring the reality of how animation studios operate and how much planning goes into these processes. Naturally, there was still no DBS continuation after the Wan Piss movie came out. The anime still hasn't return yet almost 4 (FOUR) years later, but Reekdom still proclaims he wasn't wrong and didn't lie.")
+def on_button_click6():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "The FilenameBITCH is the name of a particularly autistic Bejitafag who is known for posting long fucking rants that nobody reads. His MO is posting edited manga panels with extremely long filenames, that are usually aimed at humiliating VASSALku and make the Prince look good. Contrary to the belief of some fucking newSHITTERs, The Belgian and the filenamefag are not the same person. In fact, the Filenamefag is even older, but is not as well known because he only ever enters half-dead threads after they've hit the bump limit for some FUCKING reason, which may be one reason why Tardkurats never started FOAZing out over him like they did with The Belgian. His reputation took a nosedive when it was discovered that he's a fucking REDDITBITCH and gamefaq-BITCH who tries to spread his propaganda outside of the hallowed halls. It is also believed that he is one of the Tienfags.")
+def on_button_click7():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "Moeshit is the one and only threat to the Chads of /a/. As moeshit contrasts naturally with chadness and manliness, like a Yin and Yang, it is only natural that /dbs/, being the most masculine and dominant place on the entire Internet, would find itself to be its most extreme opposite. Moeshit invading the sacred realm of /dbs/ is therefore considered the ultimate poison, so vile that Characterfags will even stop their usual shitflinging and team up against this menace. However, despite its poisonous influence, moeshitters routinely get BTFO and forced on their knees. After all, if /dbs/ didn't win all the time, they wouldn't be the chads and rulers of /a/ (which they are). Vanquishing moeshit is a thread warden's number one priority. Posts with moeshit pictures attached will commonly get a Did NOT read response by the denizens of /dbs/. This is because anything a moeshitter can say is obviously irrelevant, because he is a moeshitter, and thus there is no need to communicate, except to humiliate him.")
+def on_button_click8():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "Cooler クウラ 'Kura', Coora or DISCORDler, is the titular, very handsome main antagonist in the movie Dragon Ball Z: Cooler's Revenge and serves as the main antagonist again in Dragon Ball Z: The Return of Cooler. He is the better, stronger, faster, and more taller older brother of Frieza (pathetic manlet sissy) and the eldest son of King Cold. Like his brother, he can push his body through transformations. Also, like Frieza, Cooler possesses a biomechanical incarnation, but this is gained from merging with the Big Gete Star.")
+def on_button_click9():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "/Dbs/ is a well know battle field, featuring diverse characterfag factions, from the most relevant characters(Goku, Broly, Jiren...) to the most pathetic ones(Caulifla, Kale, Kefla...). Whoever, there was one specific faction that surpassed the bottom, and this one was the Cabbagang(Holy cringe!). Beside it's name, Cabbagang is actually one single user, who is a loser that wants to self inert as Cabba just imagine himself banging the skeleton of universe 6, Caulifla. This user was a grade 5 schizo and kept spamming lots of cringe, ms-paint edits of Cabba featuring lots of headcanons, following by the same #cabbagang quote. This made Cabbagang the most cringe and pathetic faction of /dbs/, earning the mockery and disgust of everyone, including the lowlife coomers from the cutegang that he loved so much.")
+def on_button_click10():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "Marvin Abundis Ramirez, also known as El Especialista, is a fucking CHAD C to the H to the A to the D. Holy Based. In March 2018, as the anime version of the Tournament of Power was reaching its climax, many venues in Mexico were planning public viewing parties. At least one stripclub is known to have promised free lapdances if Goku would emerge as the victor in the upcoming battle between him and Jiren. In the midst of this colossal Mexican hype train, Toei and the Japanese embassy in Mexico wrote to the authorities about how these public viewings would be illegal, as they'd go against copyright law. The Mexican population was distraught over not being able to watch their favorite anime at their favorite taco house, but just in the nick of time, El Especialista stepped up. With his supreme knowledge, El Especialista quickly BTFO'd Toei and the authorities, and the Mexican taco houses were able to hold their public viewings after all.")
+def on_button_click11():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "El Hermano (エル　ハマノ Eru Hamano), also known as El Hermano de Jiren, is the strongest character in Spic Lore, and one of the only few characters capable of defeating Gohan Blanco (Completado) and El Grande Padre (Forma Verdadera). Among the many characters created through Spic Lore, El Hermano stands alone at the top, rivaled perhaps only by El Especialista. He is the older brother of Jiren, with a better backstory, and is far smarter, far faster, and far less reckless than his brother, whom he often calls a dimwit. El Hermano is a prodigy from El Planeta Mexicano. He is responsible for killing everyone Jiren ever loved when Jiren was a child, and later doing it again by killing his master Gicchin. El Hermano is the smartest being in existence, which is why his head is so big, to contain that massive IQ. This is why he often refers to Jiren as a dimwit, an imbecile, dunce, nincompoop, ignoramus, etc... It is rumored that El Hermano was personally trained by the Regla de Todos to surpass and replace him, a fact that El Grande Padre resented, which is why El Grande Padre killed the Regla de Todos.")
+def on_button_click12():
+    """Function to be called when the button is clicked."""
+    messagebox.showinfo("ARTICLE CONTENT", "El Grande Padre (エル・グランデー・パドレー Eru Gurandei Padorei), also known as EGP for short, is the strongest being in all of existence. He is the strongest of the Angels (by a considerable margin), who are the most powerful and highest beings in the multiverse, contested only by The One Above All and Gohan Blanco, and far surpassing the Gods of Destruction (who themselves are considered above the power of any mortal). In order to achieve his evil plans, he recruited Jiren, Rolf and Red Shaggy to do his bidding. Even though Zeno is the creator and God of the multiverse, EGP is still more powerful than him, due to Zeno being a jobber who can't follow GoD tier speeds and falling on his face like a retard when summoned by Gokek. It has long been theorized by Spics that EGP is evil and plans to usurp Zeno. They therefore think he is the final boss and only Gohan Blanco can stop him.")
+
+
+# Create the main window
+root = tk.Tk()
+root.title("The Vault - Python 3.6.0 developed by Japanga at japanga.github.io/chadsofa/")
+root.geometry("770x340") # Set window size
+root.config(bg="black")
+
+# Create a label widget
+label = tk.Label(root, text="THE VAULT - /DBS/ WIKI PYTHON-BASED TEXT ARCHIVE APPLICATION", bg="black", fg="red", font=("Helvetica", 12, "bold"))
+label.pack(pady=20) # Add padding
+
+# Create a button widget
+button = tk.Button(root, text="FOAZ", command=on_button_click, bg="black", fg="red")
+button2 = tk.Button(root, text="POAZ", command=on_button_click2, bg="black", fg="red")
+button3 = tk.Button(root, text="Jiren", command=on_button_click3, bg="black", fg="red")
+button4 = tk.Button(root, text="Romaine", command=on_button_click4, bg="black", fg="red")
+button5 = tk.Button(root, text="Reekdom", command=on_button_click5, bg="black", fg="red")
+button6 = tk.Button(root, text="Filenamebitch", command=on_button_click6, bg="black", fg="red")
+button7 = tk.Button(root, text="Moeshit", command=on_button_click7, bg="black", fg="red")
+button8 = tk.Button(root, text="Discordler", command=on_button_click8, bg="black", fg="red")
+button9 = tk.Button(root, text="Cabbagang", command=on_button_click9, bg="black", fg="red")
+button10 = tk.Button(root, text="El Especialista", command=on_button_click10, bg="black", fg="red")
+button11 = tk.Button(root, text="El Hermano", command=on_button_click11, bg="black", fg="red")
+button12 = tk.Button(root, text="El Grande Padre", command=on_button_click12, bg="black", fg="red")
+button.pack(side=tk.LEFT)
+button.place(x=105, y=60)
+button2.pack(side=tk.LEFT)
+button2.place(x=150, y=60)
+button3.pack(side=tk.LEFT)
+button3.place(x=205, y=60)
+button4.pack(side=tk.LEFT)
+button4.place(x=250, y=60)
+button5.pack(side=tk.LEFT)
+button5.place(x=320, y=60)
+button6.pack(side=tk.LEFT)
+button6.place(x=105, y=100)
+button7.pack(side=tk.LEFT)
+button7.place(x=200, y=100)
+button8.pack(side=tk.LEFT)
+button8.place(x=105, y=140)
+button9.pack(side=tk.LEFT)
+button9.place(x=105, y=180)
+button10.pack(side=tk.LEFT)
+button10.place(x=180, y=180)
+button11.pack(side=tk.LEFT)
+button11.place(x=105, y=220)
+button12.pack(side=tk.LEFT)
+button12.place(x=180, y=220)
+try:
+    image = tk.PhotoImage(file="theVAULT.png") 
+    # For PNG, use: image = tk.PhotoImage(file="your_image.png")
+except tk.TclError as e:
+    print(f"Error loading image: {e}")
+    print("Ensure the image is in GIF or PNG format and the path is correct.")
+    root.destroy() # Close the window if image loading fails
+    exit()
+
+# Create a Label widget to display the image
+image_label = tk.Label(root, image=image)
+image_label.pack()
+image_label.place(x=340, y=140)
+
+# Keep a reference to the image to prevent garbage collection
+# This is crucial for PhotoImage objects
+image_label.image = image 
+
+# Start the main event loop
+root.mainloop()
